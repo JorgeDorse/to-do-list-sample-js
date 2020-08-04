b8d30357ecbc258b4b38ec6917e0772e0b6d4520
Jorge Dorse <jdorse@gmail.com>
2020-08-04 13:12:52 -0300

    .

--- app.js
+++ app.js
@@ -9,32 +9,63 @@ let tpl = `
 </form> 
 `
 formDiv.innerHTML = tpl
+let num		= [0,1,2,3,4,5,6,7,8,9]
+
 // let item = ""
 let editando = false
-let agregarItem = function() {
+let agregarItem = function(item) {
 	let input = document.querySelectorAll('#itemIng')
-	input = input [0]
+	input = input [0] //
+	let itemId = ""
 	let li = ` 			
 			<li>
 				<span>${input.value}</span>
+				<span style= "display: none" id= "itemId"> ${itemId.value}</span>
 				<button type="button" onclick="editarItem('${input.value}')">Editar</button> 
 				<button type="button" onclick="eliminarItem('${input.value}')">Eliminar</button>
 			</li>
 	`
-	if (editando) {
-		let listaIngresada = document.querySelectorAll ('li')
-		for (let i = listaIngresada.length - 1; i >= 0; i--) {
-			let itemLista = listaIngresada[i];
-			if (itemLista.firstElementChild.innerText == input.value){
-				itemLista.firstElementChild.innerText = input.value
+	if (!editando) {
+		itemIng.value= ""
+		//Generando un itemId unico para el item ingresado
+		let itemId = ''
+		let itemIdV = 4
+		for (let i = 1; i < itemIdV +1; i++){
+			for (let i = 1; i < 5; i++) {
+				//Elegir un indice aleatorio del diccionario
+				let rand = Math.floor( Math.random() * num.length)
+				itemId = itemId + rand   
 			}
-		console.log (itemLista.firstElementChild.innerText)}
-		editando = false
-	} else {
-		ul.innerHTML += li;}
-	
-
+			if (i < itemIdV ) {
+				itemId = itemId + "-"
+			}
+		}
+		console.log(itemId)
+		ul.innerHTML += li
+		console.log(ul)
+		
+	} 
+		else {
+			let listaIngresada = document.querySelectorAll ('li')
+			let itemLista = listaIngresada[itemId]
+			//console.log("item a modificar segun id::", itemLista.itemId)
+			// // let editarInd= indModif
+			// for (let i = listaIngresada.length - 1; i >= 0; i--) {
+			// 	let itemLista = listaIngresada[i]
+			// 	console.log ("itemLista::", itemLista);
+			// 	if (itemLista.firstElementChild.innerText == input.value){
+			// 		itemLista.firstElementChild.innerText = input.value
+				// }
+			// console.log ("mostrando:: ", itemLista.firstElementChild.innerText) 
+			editando = false
+			
+		}
 }
+
+
+
+
+
 
 let eliminarItem = function(item) {
 	let listaIngresada = document.querySelectorAll ('li')
@@ -54,10 +85,17 @@ let editarItem = function(item) {
 		if(itemLista.firstElementChild.innerText == item){
 		 itemIng.value= item
 		 editando = true
+		 let posicion = i
+		//  console.log ("posicion a editar :: ", posicion)
+		 return posicion
+		 console.log ("editar item:: ", editarItem)
 		}
 	}
+let itemAeditar = editarItem
+console.log ("editar item:: ", itemAeditar)
 
 }
+
         
       
 
